00b0216f5fb7585241054f7c560dc0bdb7e30c02
Seu_wk <865382238@qq.com>
2021-08-13 22:35:05 +0800

    Create Transformation.m

---
+++ Widget/四相机匹配/Transformation.m
@@ -0,0 +1,31 @@
+function [CAMERA] = Transformation(RT,camera,i)
+%Transformation   利用[R T]对camera12和camera34的坐标转换到同一坐标系下
+%[CAMERA12,CAMERA34] = Transformation(R0,T0,R1,T1,camera12,camera34)
+%  camera12和camera34可由csv数据文件直接得到
+%  此处对z坐标取负数
+%  i=1 N行3列/i=2 3行N列
+if isstruct(camera)           %判定是否为结构体
+    camera.X(find(camera.X==0))=nan;
+    x=camera.X;
+    x=x(:);
+    y=camera.Y;
+    y=y(:);
+    z=camera.Z;
+    z=-z(:);
+    xyz=[x,y,z]';
+else 
+    if i==1                   %i=1 N行3列/i=2 3行N列
+        xyz=camera';
+    elseif i==2
+        xyz=camera;
+    end
+end
+T=repmat(RT.T,1,size(xyz,2));
+% for i=1:size(x12,1)
+%     eval(['CAMERA12(:,',num2str(i),')=R0*xyz12(:,',num2str(i),')+T0;'])
+% end
+CAMERA = RT.R*xyz+T;
+if i==1
+    CAMERA=CAMERA';
+end
+end
